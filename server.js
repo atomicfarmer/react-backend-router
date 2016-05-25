e1583e0f080f07680f8590a9d16c9230a0548055
Richard Lehmann <rlehmann@atomicfarmer.com>
2016-05-25 18:21:18 -0400

    added handlebar templates

--- server.js
+++ server.js
@@ -4,11 +4,18 @@ var ReactRouter = require('react-router');
 var RouterContext = require('react-router').RouterContext;
 var createLocation = require('history/lib/createLocation');
 var express = require('express');
+var exphbs = require('express-handlebars');
 var routes = require('./routes')
 
 var app = express();
 
 var port = process.env.PORT || 3000;
+
+app.engine('.hbs', exphbs({extname: '.hbs'}));
+app.set('view engine', '.hbs');
+
+app.set('view engine', 'handlebars');
+app.set('views', __dirname + '/templates');
 
 app.listen(port, function () {
 	console.log('Server listening on: ' + port);
@@ -24,7 +31,10 @@ app.use((req, res) => {
 		} else if (redirectLocation) {
 			res.redirect(302, redirectLocation.pathname + redirectLocation.search)
 		} else if (renderProps) {
-			res.status(200).send(ReactDOMServer.renderToString(<RouterContext {...renderProps} />))
+
+			var reactHtml = ReactDOMServer.renderToString(<RouterContext {...renderProps} />);
+			res.status(200).render('index.hbs', {body: reactHtml});
+
 		} else {
 			res.status(404).send('Not found')
 		}
